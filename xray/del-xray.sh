009c74cf9520b1d51d0159cf19f253e9809e4599
SonzaiX シ <abhiedthea@gmail.com>
2024-11-21 16:37:43 +0700

    Update del-xray.sh

--- xray/del-xray.sh
+++ xray/del-xray.sh
@@ -4,18 +4,18 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^#&@ " "/usr/local/etc/xray/config/04_inbounds.j
 if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
 clear
 echo -e "————————————————————————"
-echo -e "Delete All Xray Account               "
+echo -e "Delete All Xray Account"
 echo -e "————————————————————————"
-echo -e "  You have no existing clients!"
+echo -e "You have no existing clients!"
 echo -e "————————————————————————"
 read -n 1 -s -r -p "Press any key to back on menu"
 del-xray
 fi
 clear
 echo -e "————————————————————————"
-echo -e "Delete All Xray Account               "
+echo -e "Delete All Xray Account"
 echo -e "————————————————————————"
-echo -e " User  Expired  "
+echo -e " User  Expired"
 echo -e "————————————————————————"
 grep -E "^#&@ " "/usr/local/etc/xray/config/04_inbounds.json" | cut -d ' ' -f 2-3 | column -t | sort | uniq
 echo ""
@@ -27,12 +27,12 @@ del-xray
 else
 exp=$(grep -wE "^#&@ $user" "/usr/local/etc/xray/config/04_inbounds.json" | cut -d ' ' -f 3 | sort | uniq)
 sed -i "/^#&@ $user $exp/,/^},{/d" /usr/local/etc/xray/config/04_inbounds.json
-rm -rf /var/www/html/xray/xray-$user.log
+rm -rf /var/www/html/xray/xray-$user.html
 rm -rf /user/xray-$user.log
 systemctl restart xray
 clear
 echo -e "————————————————————————"
-echo -e "All Xray Account Success Deleted          "
+echo -e "All Xray Account Success Deleted"
 echo -e "————————————————————————"
 echo -e " Client Name : $user"
 echo -e " Expired On  : $exp"
